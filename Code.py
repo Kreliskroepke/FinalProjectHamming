cf6d657a6b3c2a4c09d3a36e0d42997d6aa1611b
Kreliskroepke <cornelisroepke2006@gmail.com>
2026-01-08 15:41:05 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,5 +1,62 @@
 import numpy as np
 
+class Matrix:
+    def __init__(self, inputs):
+        self.inputs = np.array(inputs)
+        self.rijen = len(inputs)
+        self.kolommen = len(inputs[0])
+    
+    def __str__(self):
+        return str(self.inputs)
+    
+    def __add__(self,other):
+        if isinstance(other, Matrix):
+            if self.inputs.shape != other.inputs.shape:
+                #thanks stackexchange
+                raise ValueError("The matrices aren't the same size")
+            else: 
+                #empty matrix with the same dimensions
+                result = np.zeros(self.inputs.shape, dtype=self.inputs.dtype)
+
+                #for all rows
+                for i in range(self.inputs.shape[0]):
+                    #for all columns
+                    for j in range(self.inputs.shape[1]):
+                        result[i,j] = self.inputs[i,j] + other.inputs[i,j]
+            return result
+        else:
+            raise ValueError("The matrix can't be added due to a wrong type")
+
+    def __mul__(self, other):
+        if isinstance(other, Matrix):
+            if self.inputs.shape[1] != other.inputs.shape[0]:
+                raise ValueError("The matrices have the wrong sizes")
+            else:
+                resultaat = Matrix(np.zeros((self.rijen, other.kolommen),dtype=int))
+
+            for i in range(self.rijen):
+                for j in range(other.kolommen):
+                    som = 0
+                    for k in range(self.kolommen):
+                        som += self.inputs[i][k] * other.inputs[k][j]
+                    resultaat.inputs[i][j] = som % 2
+            return resultaat
+
+        elif isinstance(other, int):
+            resultaat = Matrix(np.zeros((self.rijen, self.kolommen),dtype=int))
+            for i in range(self.rijen):
+                for j in range(self.kolommen):
+                    resultaat.inputs[i][j] = (self.inputs[i][j] * other) % 2
+            return resultaat
+        elif isinstance(other, float):
+            resultaat = Matrix(np.zeros((self.rijen, self.kolommen),dtype=int))
+            for i in range(self.rijen):
+                for j in range(self.kolommen):
+                    resultaat.inputs[i][j] = (self.inputs[i][j] * other) % 2
+            return resultaat
+        else:
+            raise ValueError("The matrix can't be multiplied due to a wrong type")
+
 def binaryconvert(tekst):
     x = "Hello"
     nibblelist = []
