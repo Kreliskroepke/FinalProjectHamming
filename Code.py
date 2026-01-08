e98af9220ce54ca78352cfe4927241348c6a446a
Kreliskroepke <cornelisroepke2006@gmail.com>
2026-01-08 13:42:22 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -24,6 +24,19 @@ G = np.array([
     [0, 1, 0, 1, 0, 1, 0],
     [1, 1, 0, 1, 0, 0, 1]
 ])
-
 G_T = G.transpose()
-print(G_T)
+
+def MultofMWithNibble():
+    #verander dit nog naar de bit waarover we het hebben, is voorbeeld; verander ook functie input
+    bleh = '0100'
+    #turn nibble into vector 4x1
+    bleh_vec = np.array([int(char) for char in bleh])
+    templist = []
+    
+    for i in G_T:
+        asdf = 0
+        for j in range(4):
+            #1 * first bit + 1 * second bit + 0 * third bit + 1 * fourth bit = first element of new matrix
+            asdf += (i[j] * bleh_vec[j])
+        templist.append(asdf)
+    templist = [int(x) for x in templist]
