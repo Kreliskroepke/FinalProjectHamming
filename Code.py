8640082c09a3b2f140f2e63482dd1e7366ab19da
Kreliskroepke <cornelisroepke2006@gmail.com>
2026-01-08 12:11:37 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,5 +1,12 @@
 x = "Hello"
+nibblelist = []
+temp = ""
 
 for char in x:
     b = ''.join(format(ord(char), '08b') for char in x)
-print(b)
+for char in b:
+    temp += char
+    if len(temp) == 4:
+        nibblelist.append(temp)
+        temp = ""
+print(nibblelist)
