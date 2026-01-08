768f691e1083913a1a07f3dbef02e46d8f7d139b
Kreliskroepke <cornelisroepke2006@gmail.com>
2026-01-08 12:16:10 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,12 +1,16 @@
-x = "Hello"
-nibblelist = []
-temp = ""
+def binaryconvert(tekst):
+    x = "Hello"
+    nibblelist = []
+    temp = ""
 
-for char in x:
-    b = ''.join(format(ord(char), '08b') for char in x)
-for char in b:
-    temp += char
-    if len(temp) == 4:
-        nibblelist.append(temp)
-        temp = ""
-print(nibblelist)
+    #turn it into binary, text -> ASCII -> binary
+    for char in x:
+        b = ''.join(format(ord(char), '08b') for char in x)
+
+    #chop into segments of 4 and put in a list
+    for char in b:
+        temp += char
+        if len(temp) == 4:
+            nibblelist.append(temp)
+            temp = ""
+    print(nibblelist)
