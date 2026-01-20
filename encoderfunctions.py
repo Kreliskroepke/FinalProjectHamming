4323249117298c115d78ddbcee0ae1c7c0d9bac9
betsplie <kroonnc@gmail.com>
2026-01-20 11:37:16 +0100

    Update encoderfunctions.py

--- encoderfunctions.py
+++ encoderfunctions.py
@@ -1,18 +1,5 @@
-def encode(tekst):                        #input = str
-    codemessages = []
-    knabbellijst = binaryconvert(tekst)
-    for i in knabbellijst:
-        if G_T.kolommen != len(i):
-            raise ValueError("The length of the knabbel doesn't coincide with the dimensions of G")
-        else:
-            p_vector = Matrix([[int(char)] for char in i])
-            codemessage = G_T * p_vector
-            codemessages.append(codemessage)
-    return codemessages                #output codemessages = list of Matrices IS DIT HANDIG?
- 
-#dit zit boven de encode want hij kent binaryconvert(tekst) anders niet
-#takes the initial input, and turns it into a list of nibbles
 def binaryconvert(tekst):                #input = str
+    """takes text as input and returns a list of binary messages"""
     nibblelist = []
     bit_base = 4 #dit moet in fase 2 afhankelijk worden van k
     
@@ -23,3 +10,16 @@ def binaryconvert(tekst):                #input = str
             nibble = [int(c) for c in nibble]
             nibblelist.append(nibble)
     return nibblelist                    #output nibblelist = [[ints],..]
+
+def encode(tekst):      #input = str
+    """takes a message, translates it to binary, and returns a list with codemessages"""
+    codemessages = []
+    knabbellijst = binaryconvert(tekst)
+    for i in knabbellijst:
+        if G_T.kolommen != len(i):
+            raise ValueError("The length of the knabbel doesn't coincide with the dimensions of G")
+        else:
+            p_vector = Matrix([[int(char)] for char in i])
+            codemessage = G_T * p_vector
+            codemessages.append(codemessage)
+    return codemessages                #output codemessages = list of Matrices IS DIT HANDIG?
