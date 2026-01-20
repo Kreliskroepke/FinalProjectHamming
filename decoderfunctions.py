f583e1db2c26d0ea455e7702d5777555c4d479c6
betsplie <kroonnc@gmail.com>
2026-01-20 11:43:20 +0100

    Update decoderfunctions.py

--- decoderfunctions.py
+++ decoderfunctions.py
@@ -1,25 +1,20 @@
-def decode(codemessages):            #input is nu list of matrices
-    H = G.parity()  
-    R = Matrix([  
-        [0, 0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0, 0, 1]
-    ])
-    allnibbles = []
+def is_zero_matrix(mat):        #input= matrix
+    """replaces np.all"""
+    for row in mat.vorm:
+        for value in row:
+            if value != 0:
+                return False
+    return True
 
-    codemessages = Random(codemessages)
-    for codemessage in codemessages:
-        vector = H * codemessage.transpose()
-        if not is_zero_matrix(vector):
-            codemessage = correct(codemessage, H)   
-        receivednibble = R * codemessage
-        allnibbles.append(receivednibble)
-
-    receivedmessage = convert_to_string(allnibbles)  
-    return receivedmessage
+def position(vector):     #input = matrix
+    """takes vector, returns binary number that it forms"""
+    num = vector.vorm[0][0]    #je kunt matrix niet indiceren, dus heb er vector.vorm van gemaakt
+    for i in vector:        #i is een list, geen bit, dus dit gaat niet goed nu
+        num = num*2 + i
+    return (num)
 
 def correct(codemessage, H):     #input = matrix, matrix
+    """checks if message has errors and corrects them"""
     codemessage = codemessage.transpose())     #codemessage is al matrix
     error_position = position(H*codemessage)
     codemessage.vorm[error_position-1] = 1 -codemessage.vorm[error_position-1] 
@@ -29,15 +24,9 @@ def correct(codemessage, H):     #input = matrix, matrix
     else:
         return codemessage
 
-  def position(vector):     #input = matrix
-    #vector als [0,0,0]
-    num = vector.vorm[0][0]    #je kunt matrix niet indiceren, dus heb er vector.vorm van gemaakt
-    for i in vector:        #i is een list, geen bit, dus dit gaat niet goed nu
-        num = num*2 + i
-    return (num)
-
 #return the knabbels to strings of binary
 def convert_to_string(allknabbels):        #input = allknabbels is list met matrices
+    """converts binary into text"""" 
     decodedmessage = ""
     tempmessage = ""
     binarymessage = ""
@@ -55,10 +44,24 @@ def convert_to_string(allknabbels):        #input = allknabbels is list met matr
             continue
     return decodedmessage        #output = nu lege string volgens mij?
 
-#replacement of np.all() 
-def is_zero_matrix(mat):        #input= matrix
-    for row in mat.vorm:
-        for value in row:
-            if value != 0:
-                return False
-    return True
+def decode(codemessages):            #input is nu list of matrices
+    """takes list of codemessages, checks errors, corrects and translates to text, returns text message"""
+    H = G.parity()  
+    R = Matrix([  
+        [0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 1]
+    ])
+    allnibbles = []
+
+    codemessages = codemessages
+    for codemessage in codemessages:
+        vector = H * codemessage.transpose()
+        if not is_zero_matrix(vector):
+            codemessage = correct(codemessage, H)   
+        receivednibble = R * codemessage
+        allnibbles.append(receivednibble)
+
+    receivedmessage = convert_to_string(allnibbles)  
+    return receivedmessage
