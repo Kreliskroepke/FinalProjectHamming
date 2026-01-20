2e759de2be1cab79e3c7151c3eec3b8b6e0caeb5
betsplie <kroonnc@gmail.com>
2026-01-20 11:05:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ def main():
     #verstuur/test de code door walkie_talkie(codemessages) 
     receivedmessage = decode(codemessages)
     return receivedmessage
+
+    G = Matrix([
+        [1, 1, 1, 0, 0, 0, 0],
+        [1, 0, 0, 1, 1, 0, 0],
+        [0, 1, 0, 1, 0, 1, 0],
+        [1, 1, 0, 1, 0, 0, 1]
+    ])
+    G_T = G.transpose()
