db3b3a8c24188631d51093a1533b46b6ffd64cf8
betsplie <kroonnc@gmail.com>
2026-01-20 11:03:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+def main():
+    message = "hi"
+    codemessages = encode(message) 
+    #verstuur/test de code door walkie_talkie(codemessages) 
+    receivedmessage = decode(codemessages)
+    return receivedmessage
