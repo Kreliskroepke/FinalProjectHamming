ec80621ad7e00e9f46c1245ef1d8cf577b1bcfce
betsplie <kroonnc@gmail.com>
2026-01-20 17:41:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,18 @@ def main():
     G_t = G.transpose()
     H = H_matrix(r)
     R = R_matrix(r)
-    
+
+    """
+    LET OP ------ remove this comment when code is finished -----
+    for code developing purposes, these are the matrices that the codes above produce (conform wikipedia):
+    G = [[1, 1, 1, 0, 0, 0, 0], [1, 0, 0, 1, 1, 0, 0], [0, 1, 0, 1, 0, 1, 0], [1, 1, 0, 1, 0, 0, 1]]
+    G_t = [[1, 1, 0, 1], [1, 0, 1, 1], [1, 0, 0, 0], [0, 1, 1, 1], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+    H = [[1, 0, 1, 0, 1, 0, 1], [0, 1, 1, 0, 0, 1, 1], [0, 0, 0, 1, 1, 1, 1]]
+    R = [[0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 1]]
+
+    LET OP MATRIX G IS GEEN GLOBAL VARIABLE MEER DUS JE MOET HEM MEEGEVEN MET DE CODE, HEB IK HIER IN MAIN EN (VOLGENS MIJ) OP ALLE RELEVANTE PLEKKEN NU OOK GEDAAN
+    """
+
     codemessages = encode(message, G_t) 
     codemessages = random_error(codemessages) 
     receivedmessage = decode(codemessages, H, R)
@@ -22,7 +33,8 @@ def main():
 main()
 
 """
-#dit is wat de Harvard filmpjes guy doet om de code goed importeerbaar te maken en later unit tests te kunnen draaien
+#dit is wat de Harvard filmpjes guy doet om de code goed importeerbaar te maken en later unit tests te kunnen draaien:
+
 if __name__ == "__main__":
     main()
 """
