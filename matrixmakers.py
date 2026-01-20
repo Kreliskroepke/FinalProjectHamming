289f24dcf3bd3e74e6242eee2261f1ccff9ea147
betsplie <kroonnc@gmail.com>
2026-01-20 17:33:09 +0100

    Update matrixmakers.py

--- matrixmakers.py
+++ matrixmakers.py
@@ -1,11 +1,13 @@
 from Matrixclass import *
 
 def G_matrix(r=3):
-    """makes generator matrix based on number of parity bits"""
-    n = 2**r -1	#n is lengte codewoord
-    k = n - r 	#k is aantal databits
+    """makes kxn generator matrix based on number of parity bits"""
+    n = 2**r -1	#n is length of codemessage
+    k = n - r 	#k is number of databits / length of knabbel
 
+    #op elke 2**p in een codemessage komt een parity bit van 0 tot r-1 (dus bij r=3 komt er parity op 2**0, 2**1 en 2**2)
     parity_positions = [2**p for p in range(r)]
+    #op plekken waar geen parity bit staat komt een databit
     data_positions = [d for d in range(1,n+1) if d not in parity_positions]
     
     G = Matrix.nulmatrix_maker(k,n)
@@ -22,13 +24,13 @@ def G_matrix(r=3):
     return G
 
 def H_matrix(r=3):
-    """makes parity-check matrix based on number of parity bits"""
+    """makes rxn parity-check matrix based on number of parity bits"""
     n = 2**r -1
 
     H = Matrix.nulmatrix_maker(r,n)
     for column in range(1,n+1):
         bits = format(column, f"0{r}b")
-        #draait de bit om zodat de matrix goed komt, deze code kan vast handiger, maar dit werkt...
+        #draait de bit om zodat de matrix wikipedia-stijl komt, deze code kan vast handiger, maar dit werkt...
         bits = bits[::-1] 
         for row in range(r):
             H[row][column-1] = int(bits[row])
@@ -36,7 +38,7 @@ def H_matrix(r=3):
     return H
 
 def R_matrix(r=3):
-    """makes decoder matrix based on number of parity bits"""
+    """makes kxn decoder matrix based on number of parity bits"""
     n = 2**r -1
     k = n - r
     
