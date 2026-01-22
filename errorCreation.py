b11674f6dbde397097bbdfadfc342d55025a9a3d
Kreliskroepke <cornelisroepke2006@gmail.com>
2026-01-22 21:18:49 +0100

    Update errorCreation.py

--- errorCreation.py
+++ errorCreation.py
@@ -1,27 +1,27 @@
 from Matrixclass import Matrix
 import random
 
-def random_error(codemessages): #hoofdletter maakt het class, random overschrijft de andere random, dus heb m random_error genoemd en aangepast in main
+def random_error(codemessages):
     """simulates errors in code that happen because of noise"""
-    # 1 in 10 accurence of 2 mistakes
+    #1 in 10 occurence of 2 mistakes
     twochanges = random.randint(0,9)   
     codemessage = random.choice(codemessages)
-    #when 2 mistakes accure somewhere in the list of codes 
+    #when 2 mistakes occur somewhere in the list of codes 
     if twochanges == 0 and codemessage.kolommen >= 2:
-        #locatie willekeurig kiezen 
-        #codemessage = codemessages[random.randint(0, len(codemessages)-1)] #ik heb dit aangepast zodat het met andere r-waarden werkt
+        #randomly chose error location
+        #codemessage = codemessages[random.randint(0, len(codemessages)-1)]
         places = random.sample(range(0,codemessage.kolommen), 2) 
 
-        #aanpassen op 2 plaatsen
+        #Fix the error on 2 places
         codemessage.vorm[places[0]][0] = 1 - codemessage.vorm[places[0]][0]   
         codemessage.vorm[places[1]][0] = 1 - codemessage.vorm[places[1]][0]
         return codemessages 
 
-    #when only 1 mistake accures in each code
+    #when only 1 mistake occurs in each code:
     else:
-        codemessage = random.choice(codemessages)
-        place = random.randint(0, codemessage.kolommen-1)    
-        codemessage.vorm[place][0] = 1 - codemessage.vorm[place][0]
+        for codemessage in codemessages: 
+            place = random.randint(0, codemessage.kolommen-1)    
+            codemessage.vorm[place][0] = 1 - codemessage.vorm[place][0]
 
-        # output list of matrices 
+        #output is list of matrices 
         return codemessages
