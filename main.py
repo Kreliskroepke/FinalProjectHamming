c6ef1f5c7ee6e4b3b7b972a6854c0e98deef0b35
betsplie <kroonnc@gmail.com>
2026-01-29 16:56:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from matrixMakers import * #contains: G_matrix, H_matrix, R_matrix
 from window import windowmaker
 
 def main():
-    """Main code, mainly for tests"""
-    message = "hi"
-    r = 3 #number of paritybits 
+    """Runs the GUI version of the program"""
+    r = 3                               #number of paritybits, adjust this number to run different Hamming(n,k) codes
+    windowmaker(r)
+
+def runHamming():
+    """Runs without opening GUI"""
+    message = "bit+wise-operator101"    #make sure this message corresponds with the message in bitwise.py
+    r = 3                               #make sure r (the number of parity bits) corresponds with the value of r in bitwise.py 
     G = G_matrix(r)
     G_t = G.transpose()
     H = H_matrix(r)
@@ -17,8 +22,6 @@ def main():
     codemessages = encode(message, G_t) 
     codemessages = randomError(codemessages) 
     receivedmessage = decode(codemessages, H, R)
-
-    windowmaker(r)
     
     return receivedmessage
 
