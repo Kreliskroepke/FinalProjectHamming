98aade4451136299ac56267e28c837201a96d732
betsplie <kroonnc@gmail.com>
2026-01-29 11:02:29 +0100

    Update errorCreation.py

--- errorCreation.py
+++ errorCreation.py
@@ -1,15 +1,12 @@
 from Matrixclass import Matrix
 import random
 
-def random_error(codemessages):
+def randomError(codemessages):
     """simulates errors in code that happen because of noise"""
     #1 in 10 occurence of 2 mistakes
     twochanges = random.randint(0,9)   
     codemessage = random.choice(codemessages)
-    #when 2 mistakes occur somewhere in the list of codes 
     if twochanges == 0 and codemessage.kolommen >= 2:
-        #randomly chose error location
-        #codemessage = codemessages[random.randint(0, len(codemessages)-1)]
         places = random.sample(range(0,codemessage.kolommen), 2) 
 
         #Fix the error on 2 places
@@ -23,5 +20,4 @@ def random_error(codemessages):
             place = random.randint(0, codemessage.kolommen-1)    
             codemessage.vorm[place][0] = 1 - codemessage.vorm[place][0]
 
-        #output is list of matrices 
-        return codemessages
+        return codemessages   #output is list of matrices 
