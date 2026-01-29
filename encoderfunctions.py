00f84e6f04d361eaadafadf6853f8ea9aff0788e
betsplie <kroonnc@gmail.com>
2026-01-29 10:58:02 +0100

    Update encoderfunctions.py

--- encoderfunctions.py
+++ encoderfunctions.py
@@ -1,24 +1,24 @@
 from Matrixclass import Matrix
 
-def binaryconvert(tekst, k=4):     #input = str
+def binaryconvert(text, k=4):
     """takes text as input and returns a list of binary messages"""
-    nibblelist = []
+    knabbel_list = []
     bit_base = k 
     b = k if k > 8 else 8
-    for i in tekst:
+    for i in text:
         bits = format(ord(i), f'0{b}b')
-        nibbles = [bits[j:j+bit_base] for j in range(0, b, bit_base)]
-        for nibble in nibbles:
-            nibble = [int(c) for c in nibble]
-            nibblelist.append(nibble)
-    return nibblelist     #output nibblelist = [[ints],..]
+        knabbels = [bits[j:j+bit_base] for j in range(0, b, bit_base)]
+        for knabbel in knabbels:
+            knabbel = [int(c) for c in knabbel]
+            knabbel_list.append(knabbel)
+    return knabbel_list
 
-def encode(tekst, G_t):     #input = str
+def encode(text, G_t):
     """takes a message, translates it to binary, and returns a list with codemessages"""
     codemessages = []
     k = G_t.kolommen
-    knabbellijst = binaryconvert(tekst, k)
-    for i in knabbellijst:
+    knabbel_list = binaryconvert(tekst, k)
+    for i in knabbel_list:
         if G_t.kolommen != len(i):
             raise ValueError("The length of the knabbel doesn't coincide with the dimensions of G")
         else:
@@ -27,10 +27,10 @@ def encode(tekst, G_t):     #input = str
             codemessages.append(codemessage)
     return codemessages     #output codemessages = list of matrices 
 
-def readableEncoder(nibblelist):
+def readableEncoder(knabbel_list):
     """This prints a string of binary in segments of 4, for window"""
     Readable = ""
-    for i in nibblelist:
+    for i in knabbel_list:
         for j in i:
             Readable += str(j)
     return ' '.join(Readable[i:i+4] for i in range(0,len(Readable),4))
