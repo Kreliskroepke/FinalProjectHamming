54cbbbff312a28514dba16b23ecc8a35560b7ae9
betsplie <kroonnc@gmail.com>
2026-01-29 11:18:44 +0100

    Update test_hamming.py

--- test_hamming.py
+++ test_hamming.py
@@ -1,13 +1,9 @@
-from decoderfunctions import * #contains: decode, correct, position, convert_to_string, is_zero_matrix
-from encoderfunctions import * #contains: encode, binaryconvert
-from errorCreation import random_error
+from decoderFunctions import * #contains: decode, correct, position, convertToString, isZeroMatrix
+from encoderFunctions import * #contains: encode, binaryConvert
+from errorCreation import randomError
 from main import main
 from Matrixclass import Matrix
-from matrixmakers import * #contains: G_matrix, H_matrix, R_matrix
-
-"""
-To use, paste the following into the terminal: py -m pytest test_hamming.py
-"""
+from matrixMakers import * #contains: G_matrix, H_matrix, R_matrix
 
 r = 3 #change r to test different Hamming codes
 G = G_matrix(r)
@@ -15,10 +11,10 @@ G_t = G.transpose()
 H = H_matrix(r)
 R = R_matrix(r)
 
-def test_random_error():
-    assert random_error(encode("hi", G_t)) != encode("hi", G_t)
-    assert random_error(encode("roomba is stuck on the carpet", G_t)) != encode("roomba is stuck on the carpet", G_t)
-    assert random_error(encode("hi", G_t)) != encode("hi", G_t)
+def test_randomError():
+    assert randomError(encode("hi", G_t)) != encode("hi", G_t)
+    assert randomError(encode("roomba is stuck on the carpet", G_t)) != encode("roomba is stuck on the carpet", G_t)
+    assert randomError(encode("hi", G_t)) != encode("hi", G_t)
 
 def test_decode():
     assert decode(encode("hello world", G_t), H, R) == "hello world"
@@ -28,8 +24,8 @@ def test_decode():
     assert decode(encode("1200+234567=9999887?", G_t), H, R) == "1200+234567=9999887?"
 
 def test_entire_code():
-    assert decode(random_error(encode("hej hej", G_t)), H, R) == "hej hej"
-    assert decode(random_error(encode("don't stop me now!", G_t)), H, R) == "don't stop me now!"
-    assert decode(random_error(encode("0", G_t)), H, R) == "0"
-    assert decode(random_error(encode("1a+2bc=3pi", G_t)), H, R) == "1a+2bc=3pi"
-    assert decode(random_error(encode("programming for mathematics is our #1 course", G_t)), H, R) == "programming for mathematics is our #1 course"
+    assert decode(randomError(encode("hej hej", G_t)), H, R) == "hej hej"
+    assert decode(randomError(encode("don't stop me now!", G_t)), H, R) == "don't stop me now!"
+    assert decode(randomError(encode("0", G_t)), H, R) == "0"
+    assert decode(randomError(encode("1a+2bc=3pi", G_t)), H, R) == "1a+2bc=3pi"
+    assert decode(randomError(encode("programming for mathematics is our #1 course", G_t)), H, R) == "programming for mathematics is our #1 course"
